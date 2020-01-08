2e22886a9c99a6c260c7ab5d68a3d819fcbdc524
zhaoxingrong <zhaoxingrong@outlook.com>
2020-01-08 17:58:00 +0800

    manager

---
+++ manager.py
@@ -0,0 +1,20 @@
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from info import create_app, db, models  # 这里导入models仅仅是为了在迁移时，让manage知道models的存在
+
+# 创建app
+app = create_app('dev')
+
+# 创建脚本管理器对象
+manager = Manager(app)
+# 让迁移和app和数据库建立管理
+Migrate(app, db)
+# 将数据库迁移的脚本添加到manager
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    # print(app.url_map)
+    # app.run()
+    manager.run()
+
+
