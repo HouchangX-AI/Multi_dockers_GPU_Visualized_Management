f695df495a149fc3cda62fc4414a22a666f3ef1e
zhaoxingrong <zhaoxingrong@outlook.com>
2020-01-09 14:34:51 +0800

    解决排序问题

--- info/modules/gpu/views.py
+++ info/modules/gpu/views.py
@@ -10,21 +10,21 @@ def rate():
     # 对取到的字典根据键的时间进行排序
     rate_list = sorted(rate_dict.items(), key=lambda x: x[0], reverse=False)
     # print(rate_list)
-    rate_dict = {i[0]: i[1] for i in rate_list}
+    # rate_dict = {i[0]: i[1] for i in rate_list}
     # print(rate_dict)
     x_list = []
     y_list_1, y_list_2, y_list_3, y_list_4, y_list_5, y_list_6, y_list_7, y_list_8, y_list_all = [], [], [], [], [], [], [], [], []
-    for k, v in rate_dict.items():
-        x_list.append(k)
-        y_list_1.append(v.get('rate_1'))
-        y_list_2.append(v.get('rate_2'))
-        y_list_3.append(v.get('rate_3'))
-        y_list_4.append(v.get('rate_4'))
-        y_list_5.append(v.get('rate_5'))
-        y_list_6.append(v.get('rate_6'))
-        y_list_7.append(v.get('rate_7'))
-        y_list_8.append(v.get('rate_8'))
-        y_list_all.append(v.get('all_mem_used_rate'))
+    for i in rate_list:
+        x_list.append(i[0])
+        y_list_1.append(i[1].get('rate_1'))
+        y_list_2.append(i[1].get('rate_2'))
+        y_list_3.append(i[1].get('rate_3'))
+        y_list_4.append(i[1].get('rate_4'))
+        y_list_5.append(i[1].get('rate_5'))
+        y_list_6.append(i[1].get('rate_6'))
+        y_list_7.append(i[1].get('rate_7'))
+        y_list_8.append(i[1].get('rate_8'))
+        y_list_all.append(i[1].get('all_mem_used_rate'))
     data = {
         'x_list': x_list,
         'y_list_1': y_list_1,
